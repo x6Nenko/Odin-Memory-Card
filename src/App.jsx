50aecf127d06c6f978fdef470c214e108d0d2952
Max I <clickkava@gmail.com>
2024-03-07 12:20:46 +0200

    Add custom hook for fetching data

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react';
 import './App.css'
+import useData from './hooks/useData';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const data = useData('https://pokeapi.co/api/v2/pokemon?limit=151&offset=0');
+  console.log(data);
+
+  // start component
+  // header
+  // card
+  // result
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      Hello World!
     </>
   )
 }
