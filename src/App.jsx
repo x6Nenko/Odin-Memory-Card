716dae3b829cc217988a31489a388e4f3fe03576
Max I <clickkava@gmail.com>
2024-03-14 22:11:40 +0200

    usePokemonData now relies on useEffect
    App now can take full pokemon data and use it.

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,23 @@ function App() {
   const data = useData('https://pokeapi.co/api/v2/pokemon?limit=151&offset=0');
   // console.log(data);
 
-  const selectedPokemons = data ? pickRandomPokemons(data.results, 3) : null;
+  const [selectedPokemons, setSelectedPokemons] = useState(null);
+  // const selectedPokemons = data ? pickRandomPokemons(data.results, 3) : null;
+
+  // mb use ref here?
+  useEffect(() => {
+    if (data) {
+      setSelectedPokemons(pickRandomPokemons(data.results, 3));
+    }
+  }, [data]);
+
+  console.log(selectedPokemons);
+
   const pokemonsData = usePokemonData(selectedPokemons);
   console.log(pokemonsData);
 
 
-  // fetch pokemon data
+  // fetch pokemon data - try to rewrite it using useEffect 
   // start component
   // header
   // card
--- src/hooks/usePokemonData.js
+++ src/hooks/usePokemonData.js
@@ -1,12 +1,54 @@
-function usePokemonData(pickedPokemons) {
-  async function getPoke(url) {
-    const response = await fetch(url);
-    return await response.json();
-  }
+// function usePokemonData(pickedPokemons) {
+//   async function getPoke(url) {
+//     const response = await fetch(url);
+//     return await response.json();
+//   }
 
-  if (pickedPokemons) {
-    return Promise.all(pickedPokemons.map(poke => getPoke(poke.url))).then(result => console.log(result));
-  }
+//   if (pickedPokemons) {
+//     return Promise.all(pickedPokemons.map(poke => getPoke(poke.url))).then(result => console.log(result));
+//   }
+// }
+
+// export default usePokemonData;
+
+import { useState, useEffect } from 'react';
+
+function usePokemonData(pickedPokemons) {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    let ignore = false;
+    console.log("started usePokemonData");
+
+    async function getPoke(pickedPokemons) {
+      const responses = await Promise.all(pickedPokemons.map(poke => fetch(poke.url)));
+      console.log(responses);
+      const result = await Promise.all(responses.map(response => response.json()));
+      console.log(result);
+
+      return result
+    }
+
+    if (pickedPokemons && pickedPokemons.length > 1) {
+      console.log(pickedPokemons);
+      console.log("now i call getPoke");
+      getPoke(pickedPokemons).then(result => {
+        if (!ignore) {
+          setData(result);
+          console.log(result);
+        }
+      });
+
+      return () => {
+        ignore = true;
+      };
+    }
+  }, [pickedPokemons]);
+
+  console.log("===================");
+  console.log(pickedPokemons);
+  console.log("===================");
+  return data;
 }
 
 export default usePokemonData;
